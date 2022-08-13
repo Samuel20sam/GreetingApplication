c12be2d75c1b8d1953ccb63d0b8a5e1983de5e94
Samuel_Elijah <samuel20sam@gmail.com>
2022-08-13 12:08:27 +0530

    Adding: Greeting Controller to send message from service layer

--- GreetingApp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ GreetingApp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.greetingapp.controller;
 
+import com.bridgelabz.greetingapp.service.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,10 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
+    final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     //UC1
     @GetMapping("/getMap")
     public String getMessage() {
-        return "Hello World";
+        return "Hello World this is Samuel  from control layer";
     }
 
     @GetMapping("/getByParam")
@@ -22,5 +29,11 @@ public class GreetingController {
     @GetMapping("/getPath/{name}")
     public String getPath(@PathVariable String name) {
         return "Hello" + name;
+    }
+
+    //UC2
+    @GetMapping("/getbyservice")
+    public String getbyservice(){
+        return greetingService.getmessage();
     }
 }
