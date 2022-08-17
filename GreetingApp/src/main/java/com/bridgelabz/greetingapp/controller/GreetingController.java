0827116e946bfd388b16c64c26de88fbc633fb2f
Samuel_Elijah <samuel20sam@gmail.com>
2022-08-17 13:38:51 +0530

    Adding: Greeting App with Adding, editing and deleting function

--- GreetingApp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ GreetingApp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,52 +1,71 @@
 package com.bridgelabz.greetingapp.controller;
 
-import com.bridgelabz.greetingapp.service.GreetingService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.bridgelabz.greetingapp.model.Greeting;
+import com.bridgelabz.greetingapp.model.User;
+import com.bridgelabz.greetingapp.service.IGreetingService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class GreetingController {
 
-    final GreetingService greetingService;
+    private final IGreetingService greetingService;
 
-    public GreetingController(GreetingService greetingService) {
+    public GreetingController(IGreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    //UC1
-    @GetMapping("/getMap")
-    public String getMessage() {
-        return "Hello World this is Samuel  from control layer";
+//    * @Func :Returning JSON Object
+//    * @Param:Adding first name ,last name
+//    * In "params"-POSTMAN
+//    * {
+//    *     firstName=Samuel
+//    *     lastName=Elijah
+//    * }
+//    * @URL: <a href="http://localhost:8080/home">...</a>  -Postman-GET
+//    * @return: hello
+
+    @GetMapping(value={" ", "/" , "/home"})
+    public Greeting greeting(@RequestParam(value = "firstName", defaultValue = "World") String firstName,
+                             @RequestParam(value = "lastName", defaultValue = "") String lastName) {
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        return greetingService.addGreeting(user);
     }
 
-    @GetMapping("/getByParam")
-    public String getByParam(@RequestParam String name) {
-        return "Hello" + name;
+//   * @Func :Ability To Find By id - for eg:"id=2"
+//   * @URL: http://localhost:8080/path/2
+//   * @Return: Value at id 2
+    @GetMapping("/path/{id}")
+    public Greeting getElementById(@PathVariable Long id){
+        return greetingService.getGreetingById(id);
+    }
+//   * @Func :Ability To List All messages
+//   * @URL: http://localhost:8080/all
+//   * @Return: All elements in repository
+
+    @GetMapping("/all")
+    public List<Greeting> getAll(){
+        return greetingService.getAll();
     }
 
-    @GetMapping("/getPath/{name}")
-    public String getPath(@PathVariable String name) {
-        return "Hello" + name;
+//     * @Func :Ability To Edit Messages in Repository - - for eg:"id=2"
+//     * @URL: http://localhost:8080/edit/2
+//     * @Return: Edits position id 2
+
+    @PutMapping("/edit/{id}")
+    public Greeting editGreetingById(@PathVariable long id, @RequestParam String name){
+        return greetingService.editGreetingById(id, name);
     }
 
-    //UC2
-    @GetMapping("/getbyservice")
-    public String getbyservice(){
-        return greetingService.getmessage();
-    }
+//     * @Func :Ability To Delete Messages in Repository - for eg:"id=2"
+//     * @URL: http://localhost:8080/delete/2
+//     * @Return: Deletes the position id 2
 
-    //UC3
-    @GetMapping("/hello")
-    public String sayPosting(@RequestParam(required = false) String firstName,
-                             @RequestParam(required = false) String lastName) {
-        if (lastName == null) lastName = "";
-        else if (firstName == null) firstName = "";
-        else if (firstName == null && lastName == null) {
-            firstName = "";
-            lastName = "";
-        }
-        return greetingService.sayHelloByName(firstName, lastName);
+    @DeleteMapping("/delete/{id}")
+    public List<Greeting> deleteGreetingById(@PathVariable Long id){
+        return greetingService.deleteGreetingById(id);
     }
 }
--- GreetingApp/src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
+++ GreetingApp/src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
@@ -1,14 +1,48 @@
 package com.bridgelabz.greetingapp.service;
 
+import com.bridgelabz.greetingapp.model.Greeting;
+import com.bridgelabz.greetingapp.model.User;
+import com.bridgelabz.greetingapp.repository.GreetingRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Service
-public class GreetingService {
-    public String getmessage() {
-        return "Hello World This is Samuel from Service layer";
+public class GreetingService implements IGreetingService{
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    final GreetingRepository greetingRepository;
+
+    public GreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
     }
 
-    public String sayHelloByName(String firstName, String lastName) {
-        return "Hello "+firstName+" "+lastName;
+    @Override
+    public Greeting addGreeting(User user) {
+        String message = String.format(template, (user.toString().isEmpty()) ? "Hello World" : user.toString());
+        return greetingRepository.save(new Greeting(counter.incrementAndGet(), message));
+    }
+    @Override
+    public Greeting getGreetingById(long id) {
+        return greetingRepository.findById(id).get();
+    }
+    @Override
+    public List<Greeting> getAll() {
+        return greetingRepository.findAll();
+    }
+    @Override
+    public Greeting editGreetingById(long id, String name) {
+        Greeting greeting = greetingRepository.findById(id).get();
+        greeting.setMessage(name);
+        greetingRepository.save(greeting);
+        return greeting;
+    }
+    @Override
+    public List<Greeting> deleteGreetingById(Long id) {
+        greetingRepository.deleteById(id);
+        return greetingRepository.findAll();
     }
 }
